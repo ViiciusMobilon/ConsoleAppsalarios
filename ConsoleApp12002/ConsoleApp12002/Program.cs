b7579636ef7d51ec1d64d37767bf7a026af3a35f
Usuario <Usuario@CPD03-M12>
2024-03-26 12:07:45 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp12002/ConsoleApp12002/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12002
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("olá Mundo");
+            Console.WriteLine(2);
+            Console.WriteLine(2.5);
+            Console.WriteLine(2+2);
+            Console.WriteLine("resultado é " + 2 + 2);
+            Console.WriteLine("resultado é " + (2 + 2));
+            Console.WriteLine("resultado é " + 2 * 2);
+            Console.WriteLine( 2 + 2 + " é o resultado  " );
+            Console.Beep();
+            Console.Read();
+
+
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String nome;
+
+            Console.WriteLine("Digite seu nome");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("O nome digitado foi:" + nome);
+
+            Console.ReadKey();
+ 
+            Console.WriteLine("")
+    
+        }   
+    }
+}
