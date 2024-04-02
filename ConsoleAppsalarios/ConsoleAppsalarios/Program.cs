5dc76f289c224fbfffc2bd98133487760285794f
Usuario <Usuario@CPD03-M12>
2024-04-02 12:19:33 -0300

    20423

---
+++ ConsoleAppsalarios/ConsoleAppsalarios/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppsalarios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Se você deseja começar digite sim");
+            string sim = Console.ReadLine().ToLower();
+
+            Console.WriteLine("Digite se vocâ e assalariado, comissioonado, Horista");
+            string tipo = Console.ReadLine().ToLower();
+
+            double fp = 0;
+            double sal2 = 0;
+
+            while (sim == "sim" )
+            {
+                switch (tipo)
+                {
+                    case "assalariado":
+                        Console.WriteLine("Qual o seu salario bruto ");
+                        double sal = double.Parse(Console.ReadLine());
+                        Console.WriteLine("Qual o desconto ");
+                        double d = double.Parse(Console.ReadLine());
+                        sal2 = sal - d;
+                        fp = fp + sal2;
+
+                        break;
+
+                    case "comissionado":
+                        Console.WriteLine("Qual a quantidade de peças produzidas ");
+                        int pecas = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Qual o valor de cada de peças produzidas ");
+                        int Ppecas = int.Parse(Console.ReadLine());
+                        sal2 = Ppecas * pecas;
+                        fp = fp + sal2;
+                        break;
+                    case "horista":
+                        Console.WriteLine("Digite a quantidades de horas trabalhadas ");
+                        int h = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Digite a quantidades de horas trabalhadas ");
+                        int ht = int.Parse(Console.ReadLine());
+                        sal2 = h * ht;
+                        fp = fp + sal2;
+                        break;
+
+                }
+            }
+            
+        }
+    }
+}
---
+++ Consolematricula/Consolematricula/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consolematricula
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int DS = 5;
+            int E = 5;
+            int M = 5;
+            int A = 5;
+            Console.WriteLine("Deseja começar");
+            string sim = Console.ReadLine().ToUpper();
+
+
+
+
+            while (sim == "SIM")
+            
+            {
+                Console.WriteLine(" DS) desemvolvimento de sistemas, (M) Mecatronica (E) Edificaçeõs (A) Administração ");
+            string curso = Console.ReadLine().ToUpper();
+                    
+                        if
+                        (curso == "DS" && DS > 0)
+                        {
+                            Console.WriteLine("Você foi cadastrado em desemvolvimento de sistemas");
+                            DS = DS - 1;
+                        }
+                        else if
+                           (curso == "E" && E > 0)
+                        {
+                            Console.WriteLine("Você foi cadastrado em Edificaçeõs");
+                            E = 5 - 1;
+                        }
+                        else if (curso == "M" && M > 0)
+                        {
+                            Console.WriteLine("Você foi cadastrado em  Mecatronica");
+                            M = 5 - 1;
+                        }
+
+                        else if (curso == "A" && A > 0)
+                        {
+                            Console.WriteLine("Você foi cadastrado em  Administração");
+                            A = A - 1;
+                        }
+                        else
+                {
+                    Console.WriteLine("Não ha mais vagas desponiveis");
+                }
+                Console.WriteLine("O numero de vagas restantes DS= "+DS+" E= "+E+" M= "+M +" A "+A);
+
+
+                Console.WriteLine("Deseja continuar ");
+                sim = Console.ReadLine().ToUpper();
+
+            }
+            Console.WriteLine("O numero de vagas restantes DS= " + DS + " E= " + E + " M= " + M + " A " + A);
+            Console.ReadKey();
+        }
+    }
+}
